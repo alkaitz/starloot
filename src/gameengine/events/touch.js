a71befd48bacbafc146f10a8ee5f09386b928143
alkaitz <alkaitz@gmail.com>
2016-12-03 12:10:59 -0800

    Clear repository

--- src/gameengine/events/touch.js
+++
@@ -1,8 +0,0 @@
-function Touch(){
-	this.init = function(){
-		var mouse = Game.mouse;
-		window.ontouchstart     = function(evt){mouse.mouseDown(evt);};
-		window.ontouchend       = function(evt){mouse.mouseUp(evt);};
-		window.ontouchmove      = function(evt){mouse.mouseMoved(evt);};
-	};
-};
--- src/gameengine/minify.bat
+++
@@ -1,2 +0,0 @@
-java -jar YuiWrapper.jar --order minheap,config,XMLReader,keyboard,mouse,touch,timer,eventmanager,resources,resourcemanager,coordinate,drawable,animation,renderer,node,entity,logic,game -o min.js --charset utf-8 --line-break 220
-
